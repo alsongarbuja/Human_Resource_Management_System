6c417843c326fc2f44b6a39eb6e87863c46a9d75
alsongarbuja <magar33alson@gmail.com>
2026-05-16 01:06:07 -0500

    db: add mysql database setting

--- Human_Resource_Management_System/settings.py
+++ Human_Resource_Management_System/settings.py
@@ -73,10 +73,22 @@ WSGI_APPLICATION = 'Human_Resource_Management_System.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  # 'default': {
+  #   'ENGINE': 'django.db.backends.sqlite3',
+  #   'NAME': BASE_DIR / 'db.sqlite3',
+  # },
+  "default": {
+    "ENGINE": "django.db.backends.mysql",
+    "NAME": "hrms_db",
+    "USER": "root",
+    "PASSWORD": "asdfAsdf#90",
+    "HOST": "localhost",
+    "PORT": "3306",
+    "OPTIONS": {
+      "charset": "utf8mb4",
+      "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+    },
+  }
 }
 
 
