4db9f33e92f4217b6bab5036644e9b8b9f64cac1
Garbuja <agarbuja1s@semo.edu>
2026-05-18 08:03:10 -0500

    feat: add models and admin panel of organization management to the system

--- organizationmanagement/admin.py
+++ organizationmanagement/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Department, Unit
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+  list_display=("name",)
+  search_fields=("name",)
+  order=("name",)
+
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
+  list_display=("name", "department",)
+  search_fields=("name",)
+  order=("name", "department",)
+  list_filter=("department",)
--- organizationmanagement/models.py
+++ organizationmanagement/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from core.models import BaseModel
+
+class Department(BaseModel):
+  name = models.CharField(max_length=100, unique=True)
+  description = models.TextField(blank=True)
+
+  def __str__(self):
+    return self.name
+
+  class Meta(BaseModel.Meta):
+    db_table = "department"
+    verbose_name = "Department"
+    verbose_name_plural = "Departments"
+
+class Unit(BaseModel):
+  name = models.CharField(max_length=50, unique=True)
+  description = models.TextField(blank=True)
+  department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.name
+
+  class Meta(BaseModel.Meta):
+    db_table = "unit"
+    verbose_name = "Unit"
+    verbose_name_plural = "Units"
