dc0ec6580a00c7660bb142765bb26e0af083480e
alsongarbuja <magar33alson@gmail.com>
2026-05-21 10:06:28 -0500

    pkg: add mssql django package

--- Human_Resource_Management_System/settings.py
+++ Human_Resource_Management_System/settings.py
@@ -93,18 +93,31 @@ DATABASES = {
   #   'ENGINE': 'django.db.backends.sqlite3',
   #   'NAME': BASE_DIR / 'db.sqlite3',
   # },
+  # 'default': {
+  #   'ENGINE': 'django.db.backends.mysql',
+  #   'NAME': 'hrms_db',
+  #   'USER': 'root',
+  #   'PASSWORD': 'asdfAsdf#90',
+  #   'HOST': 'localhost',
+  #   'PORT': '3306',
+  #   'OPTIONS': {
+  #     'charset': 'utf8mb4',
+  #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+  #   },
+  # },
+
   'default': {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'hrms_db',
-    'USER': 'root',
-    'PASSWORD': 'asdfAsdf#90',
+    'ENGINE': 'mssql',
+    'NAME': 'hrms',
+    'USER': '',
+    'PASSWORD': '',
     'HOST': 'localhost',
-    'PORT': '3306',
+    'PORT': '',
     'OPTIONS': {
-      'charset': 'utf8mb4',
-      'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-    },
-  }
+      'driver': 'ODBC Driver 17 for SQL Server',
+      'long_data_type_column': True,
+    }
+  },
 }
 
 
