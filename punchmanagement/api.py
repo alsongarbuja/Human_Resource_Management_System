e0527358228b597153e2a8707e07c93e0ec031f1
alsongarbuja <magar33alson@gmail.com>
2026-05-22 14:01:44 -0500

    fix: fix clock in create issue

--- punchmanagement/api.py
+++ punchmanagement/api.py
@@ -42,7 +42,6 @@ def clock_in_employee(request, data: ClockOutFormSchema = Form(...)):
 
     punch = PunchEntry.objects.create(
       job_profile=job_profile,
-      pay_period=current_pay_period,
       # TODO: fix this timezone
       clock_in=timezone.now()
     )
